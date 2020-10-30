f1f5efcc44cdf79ae2a9e6ea24cb242265b4cefa
Matheus0liveira <matheusfilho98.ms@gmail.com>
2020-10-29 21:53:08 -0300

    feat: create the first test

---
+++ src/pages/Home/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+
+import Home from '.';
+
+describe('<Main/>', () => {
+  it('Render the Head', () => {
+    const { container } = render(<Home />);
+
+    expect(
+      screen.getByRole('heading', { name: /Matheus/i })
+    ).toBeInTheDocument();
+  });
+});
